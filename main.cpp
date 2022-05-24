0741b8b9ed96244404b0ca49c5f1923847946f0a
Matheus Isquierdo <matheus2043@gmail.com>
2022-05-24 02:07:56 -0300

    passagem de argumentos e uso de um buffer pra evitar as linhas trocadas

--- main.cpp
+++ main.cpp
@@ -1,77 +1,106 @@
 #include <iostream>
 #include <complex>
 #include <string>
+#include <map>
 #include <mpi.h>
 
-using std::string;
 using std::cout;
+using std::string;
 
-constexpr auto image_size = 1000, max_iteration = 100;
+int image_size = 1000, max_iteration = 100;
 float offset_x = 1.0f;
 float offset_y = 0.5f;
 int world_size;
 
-typedef enum {
-    DATA_TAG, TERM_TAG, RESULT_TAG
+typedef enum
+{
+    DATA_TAG,
+    TERM_TAG,
+    RESULT_TAG
 } Tags;
 
-inline int offset2(int x, int y) {
+inline int offset(int x, int y)
+{
     return x * 3 + y;
 }
 
-void worker() {
+void worker()
+{
     MPI_Status status;
     int row;
 
-    int *output = (int *) malloc(image_size * 3 * sizeof(int));
+    int total_elements = (image_size * 3 + 1);
+    int *buffer = (int *)malloc(total_elements * sizeof(int));
+    int *output = buffer + 1;
 
     MPI_Recv(&row, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    while (status.MPI_TAG == DATA_TAG) {
-        #pragma omp parallel default(none) shared(output, status, row, offset_x, offset_y)
+    while (status.MPI_TAG == DATA_TAG)
+    {
+        buffer[0] = row;
+        #pragma omp parallel default(none) shared(buffer, output, status, row, offset_x, offset_y)
         {
             #pragma omp for
-            for (auto column = 0; column < image_size; ++column) {
+            for (auto column = 0; column < image_size; ++column)
+            {
                 std::complex<float> z, c = {
-                        (float) column * offset_x / image_size - offset_y,
-                        (float) row * offset_x / image_size - (offset_y + 0.5f)
-                };
+                                           (float)column * offset_x / image_size - offset_y,
+                                           (float)row * offset_x / image_size - (offset_y + 0.5f)};
 
                 int i = 0;
                 while (abs(z) < 2 && ++i < max_iteration)
                     z = pow(z, 2) + c;
 
-                double t = (double) i / (double) max_iteration;
+                double t = (double)i / (double)max_iteration;
 
-                int r = (int) (9 * (1 - t) * t * t * t * 255);
-                int g = (int) (15 * (1 - t) * (1 - t) * t * t * 255);
-                int b = (int) (8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
+                int r = (int)(9 * (1 - t) * t * t * t * 255);
+                int g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
+                int b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
 
-                output[offset2(column, 0)] = r;
-                output[offset2(column, 1)] = g;
-                output[offset2(column, 2)] = b;
+                output[offset(column, 0)] = r;
+                output[offset(column, 1)] = g;
+                output[offset(column, 2)] = b;
             }
         }
-        MPI_Send(&output[0], image_size * 3, MPI_INT, 0, RESULT_TAG, MPI_COMM_WORLD);
+        MPI_Send(&buffer[0], total_elements, MPI_INT, 0, RESULT_TAG, MPI_COMM_WORLD);
         MPI_Recv(&row, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     }
 
-    free(output);
+    free(buffer);
 }
 
-void leader() {
+void printRow(FILE *file, int *output) 
+{
+    for (auto column = 0; column < image_size; ++column)
+    {
+        fprintf(file, "%d\t", output[offset(column, 0)]);
+        fprintf(file, "%d\t", output[offset(column, 1)]);
+        fprintf(file, "%d\t", output[offset(column, 2)]);
+        fprintf(file, "\t");
+    }
+}
+
+void leader()
+{
     MPI_Status status;
     int count = 0;
     int row = 0;
     int rank;
+    std::map<int, int*> row_buffer;
+    int received_row;
+    int next_row = 0;
 
-    for (int i = 1; i < world_size; i++) {
+    for (int i = 1; i < world_size; i++)
+    {
         MPI_Send(&row, 1, MPI_INT, i, DATA_TAG, MPI_COMM_WORLD);
         count++;
         row++;
     }
 
-    int *output = (int *) malloc(image_size * 3 * sizeof(int));
+    int total_elements = (image_size * 3 + 1);
+    int *buffer = (int *)malloc(total_elements * sizeof(int));
+    int *output = buffer + 1;
+
     FILE *file = fopen("mandelbrot.ppm", "w");
 
     fprintf(file, "P3\n");
@@ -80,34 +109,66 @@ void leader() {
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    do {
-        MPI_Recv(&output[0], image_size * 3, MPI_INT, MPI_ANY_SOURCE, RESULT_TAG, MPI_COMM_WORLD, &status);
+    do
+    {
+        MPI_Recv(&buffer[0], total_elements, MPI_INT, MPI_ANY_SOURCE, RESULT_TAG, MPI_COMM_WORLD, &status);
+        received_row = buffer[0];
         count--;
 
-        if (row < image_size) {
+        if (row < image_size)
+        {
             MPI_Send(&row, 1, MPI_INT, status.MPI_SOURCE, DATA_TAG, MPI_COMM_WORLD);
             row++;
             count++;
-        } else {
+        }
+        else
+        {
             MPI_Send(&row, 1, MPI_INT, status.MPI_SOURCE, TERM_TAG, MPI_COMM_WORLD);
         }
 
-        for (auto column = 0; column < image_size; ++column) {
-            fprintf(file, "%d\t", output[offset2(column, 0)]);
-            fprintf(file, "%d\t", output[offset2(column, 1)]);
-            fprintf(file, "%d\t", output[offset2(column, 2)]);
-            fprintf(file, "\t");
+        if (received_row != next_row) 
+        {
+            row_buffer.insert({received_row, buffer});
+            buffer = (int *)malloc(total_elements * sizeof(int));
+            output = buffer + 1;
+        } 
+        else 
+        {
+            printRow(file, output);
+            ++next_row;
+            
+            for (auto it = row_buffer.begin(); it != row_buffer.end();/**/)
+            {
+                if (it->first == next_row)
+                {
+                    printRow(file, it->second + 1);
+                    free(it->second);
+                    row_buffer.erase(it++);
+                    
+                    ++next_row;
+                } 
+                else 
+                {
+                    break;
+                }
+            }
         }
 
         fprintf(file, "\n");
 
     } while (count > 0);
 
-    free(output);
+    free(buffer);
     fclose(file);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
+    image_size      = (argc < 2) ? 1000 : atoi(argv[1]);
+    max_iteration   = (argc < 3) ? 100  : atoi(argv[2]);
+    offset_x        = (argc < 4) ? 1.0f : atof(argv[3]);
+    offset_y        = (argc < 5) ? 0.5f : atof(argv[4]);
+    
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -122,14 +183,17 @@ int main(int argc, char **argv) {
     printf("Processor %s, rank %d out of %d processors\n",
            processor_name, world_rank, world_size);
 
-    if (world_rank == 0) {
+    if (world_rank == 0)
+    {
         leader();
-    } else {
+    }
+    else
+    {
         worker();
     }
 
     MPI_Finalize();
 }
 
-//compile: mpic++ main.cpp -o main
-//run: mpirun --hostfile hosts -np 0 ./main
+// compile: mpic++ main.cpp -o main
+// run: mpirun --hostfile hosts -np 0 ./main
